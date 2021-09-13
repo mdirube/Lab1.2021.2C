6cf4ff4afc06f7afe4bc69cd72d3a7d67b90faa5
mdirube <67482531+mdirube@users.noreply.github.com>
2021-09-13 10:39:06 -0300

    Add files via upload

---
+++ tp4_ej20.cpp
@@ -0,0 +1,49 @@
+///Ejercicio:
+///Autor:DEK
+///Fecha:
+///Comentario:
+
+# include<iostream>
+# include<cstdlib>
+
+using namespace std;
+
+/*20) Se define como divisores propios de un número a aquellos que son sus divisores
+excluyendo al número en sí mismo.
+
+Ejemplo. Los divisores propios del 4 son: 1 y 2.
+
+Ejemplo. Los divisores propios del 12 son; 1, 2, 3, 4 y 6.
+
+Se define a un número como perfecto cuando la suma de todos sus divisores propios
+coincide con el número en sí mismo.
+
+Ejemplo: 6 es número perfecto pues 1+2+3=6
+Ejemplo: 28 es número perfecto pues 1+2+4+7+14=28
+Ejemplo: 12 no es número perfecto pues 1+2+3+4+6=16
+
+Hacer un programa para ingresar un número y luego informar con un cartel aclaratorio si el
+mismo es un número perfecto o es número no perfecto.
+
+*/
+int main(){
+    int num, divisor=1, sumaDivisores=0;
+    cout<<"NUMERO: ";
+    cin>>num;
+    while(divisor<num){
+        if(num%divisor==0){
+            sumaDivisores+=divisor;
+        }
+        divisor++;
+    }
+
+	if(sumaDivisores==num){
+        cout<<"EL NUMERO INGRESADO ES UN NUMERO PERFECTO";
+	}
+	else{
+        cout<<"EL NUMERO INGRESADO NO ES UN NUMERO PERFECTO";
+	}
+	cout<<endl;
+	system("pause");
+	return 0;
+}
