2f69dfc53de8993f95f22a3f81aaca9a2b80f6db
mdirube <67482531+mdirube@users.noreply.github.com>
2021-09-27 11:42:44 -0300

    Add files via upload
    https://docs.google.com/document/d/1mmHXeK4KUqqkg4CFqrsM1GdZTX9sYXDRS_GU6yR6g3U/edit

---
+++ ejparcial2.cpp
@@ -0,0 +1,61 @@
+///Ejercicio:
+///Autor:DEK
+///Fecha:
+///Comentario:
+
+# include<iostream>
+# include<cstdlib>
+
+using namespace std;
+
+
+int main(){
+    int i, numeroJugador, cantidadPartidos, minutosJuego, pais;
+    ///punto 1
+    int contadorDeJugadores14=0;
+    ///punto 2
+    int sumaTiempos=0;
+    float tiempoPromedio;
+    ///punto 3
+    bool primero=true;
+    int menorTiempo, menorJugador, menorPais;
+
+    for(i=0;i<20;i++){
+        cout<<"NUMERO DE JUGADOR: ";
+        cin>>numeroJugador;
+        cout<<"CANTIDAD DE PARTIDOS JUGADOS: ";
+        cin>>cantidadPartidos;
+        cout<<"CANTIDAD DE MINUTOS DE JUEGO: ";
+        cin>>minutosJuego;
+        cout<<"PAIS: ";
+        cin>>pais;
+        ///punto 1
+        if(pais==14)contadorDeJugadores14++;
+        ///punto 2
+        sumaTiempos+=minutosJuego;
+        ///punto 3
+        if(primero==true && minutosJuego!=0){
+            menorTiempo=minutosJuego;
+            menorJugador=numeroJugador;
+            menorPais=pais;
+            primero=false;
+        }
+        else{
+            if(minutosJuego<menorTiempo && minutosJuego!=0){
+                menorTiempo=minutosJuego;
+                menorJugador=numeroJugador;
+                menorPais=pais;
+            }
+        }
+
+    }
+	///punto 1
+	cout<<"CANTIDAD DE JUGADORES DEL PAIS 14: "<<contadorDeJugadores14<<endl;
+	///punto 2
+	tiempoPromedio=(float)sumaTiempos/20;
+	cout<<"TIEMPO PROMEDIO: "<<tiempoPromedio<<endl;
+	cout<<"JUGADOR QUE MENOS JUGO: "<<menorJugador<<" DEL PAIS: "<<menorPais<<endl;
+	cout<<endl;
+	system("pause");
+	return 0;
+}
